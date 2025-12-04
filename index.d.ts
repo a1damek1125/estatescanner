acdbf7a1db457fdbfbf4862ae606910bc112d63a
a1damek1125 <134393868+a1damek1125@users.noreply.github.com>
2025-12-04 10:00:12 +0100

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
-export { analyze, AnalyzeOptions } from './analyze';
-export * from './definition';
-export { Reference } from './referencer/Reference';
-export { Visitor } from './referencer/Visitor';
-export { PatternVisitor, PatternVisitorCallback, PatternVisitorOptions, } from './referencer/PatternVisitor';
-export * from './scope';
-export { ScopeManager } from './ScopeManager';
-export * from './variable';
+export { AST, parse, parseAndGenerateServices, ParseAndGenerateServicesResult, } from './parser';
+export { ParserServices, ParserServicesWithTypeInformation, ParserServicesWithoutTypeInformation, TSESTreeOptions, } from './parser-options';
+export { simpleTraverse } from './simple-traverse';
+export * from './ts-estree';
+export { createProgramFromConfigFile as createProgram } from './create-program/useProvidedPrograms';
+export * from './create-program/getScriptKind';
+export { getCanonicalFileName } from './create-program/shared';
+export { typescriptVersionIsAtLeast } from './version-check';
+export * from './getModifiers';
+export { TSError } from './node-utils';
+export * from './clear-caches';
+export declare const version: string;
 //# sourceMappingURL=index.d.ts.map
